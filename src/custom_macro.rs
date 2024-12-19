8fe35487ba4ba2f80006643280f5c25f48b42ec5
luiss07 <luigi.delleva@gmail.com>
2024-12-19 14:10:23 +0100

    minor

--- src/custom_macro.rs
+++ src/custom_macro.rs
@@ -5,7 +5,7 @@ macro_rules! assert_matches_any {
             $res == $expected1 || $res == $expected2,
             "Assertion failed: `res` does not match any expected values.\n\
              Actual: `{:?}`\n\
-             Expected: `{:?}` or `{:?}`",
+             Expected: `{:?}`\nOR\n`{:?}`",
             $res,
             $expected1,
             $expected2
